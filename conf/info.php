5d8b3b57e98c231157cdcc3809d3a351345f9d65
gaetanavez19 <gaetan.avez@hetic.net>
2019-04-09 15:37:32 +0200

    conf folder with the api key avec global variables

---
+++ conf/info.php
@@ -0,0 +1,12 @@
+<?php
+
+$apikey = "4c89332638d1e1b6d095be235d544570";
+
+$sitename = "CheckMovies";
+$tagline = "Infos by CheckMovies";
+
+
+$imgurl_1 = "http://image.tmdb.org/t/p/w500";
+$imgurl_2 = "http://image.tmdb.org/t/p/w300";
+
+?>
