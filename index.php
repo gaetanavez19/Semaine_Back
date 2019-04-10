3c82c22d54740cd59f50572a2a2127d809fa7663
gaetanavez19 <gaetan.avez@hetic.net>
2019-04-10 13:44:35 +0200

    debug search

--- index.php
+++ index.php
@@ -13,10 +13,10 @@
         <button type="submit"><img src="./images/search.svg" alt="search"></button>
         <input type="search" name="search" placeholder="Ex : Aquaman" autocomplete="off" required>
       </div>
-      <select name="channel" required>
+      <!-- <select name="channel" required>
         <option value="movie" selected="selected">Movie</option>
         <option value="tv">TV Show</option>
-      </select>
+      </select> -->
       
     </form>
   </main>
--- search.php
+++ search.php
@@ -1,6 +1,5 @@
 <?php
 $input=$_GET['search'];
-$channel=$_GET['channel'];
 $search=$input;
 include "conf/info.php";
 $title = 'Search | '.$input;
@@ -10,7 +9,7 @@ include "api/api_search.php";
 	<p>Results for: <?php echo $input?></p>
     <div>
 <?php
-	if($channel=="movie"){	
+	
         foreach($search->results as $results){
 			$title = $results->title;
 			$id = $results->id;
@@ -24,32 +23,32 @@ include "api/api_search.php";
 				 }
 			echo '<li><a href="movie.php?id=' . $id . '"><img src="'.$poster.'"><p>'.$title.'</p></a></li>';
 		}
-		}
-
-	elseif($channel=="tv"){
-        foreach($search->results as $results){
-			$title 		= $results->original_name;
-			$id 		= $results->id;
-			$release	= $results->first_air_date;
-			if (!empty($release) && !is_null($release)){
-				$tempyear 	= explode("-", $release);
-				$year 		= $tempyear[0];
-				if (!is_null($year)){
-					$title = $title.' ('.$year.')';
-				}
-			}
-			$poster 	= $results->backdrop_path;
-			if (empty($poster) && is_null($poster)){
-				$poster = $pathloc.'image/no-backdrop.png';
-			} 
-			else {
-				$poster = 'http://image.tmdb.org/t/p/w300'.$poster;
-			}
-			echo '<li><a href="tvshow.php?id=' . $id . '"><img src="'.$poster.'"><h4>'.$title.'</h4></a></li>';
-			}
-		}
 		
-        ?>
+
+	// elseif($channel=="tv"){
+    //     foreach($search->results as $results){
+	// 		$title 		= $results->original_name;
+	// 		$id 		= $results->id;
+	// 		$release	= $results->first_air_date;
+	// 		if (!empty($release) && !is_null($release)){
+	// 			$tempyear 	= explode("-", $release);
+	// 			$year 		= $tempyear[0];
+	// 			if (!is_null($year)){
+	// 				$title = $title.' ('.$year.')';
+	// 			}
+	// 		}
+	// 		$poster 	= $results->backdrop_path;
+	// 		if (empty($poster) && is_null($poster)){
+	// 			$poster = $pathloc.'image/no-backdrop.png';
+	// 		} 
+	// 		else {
+	// 			$poster = 'http://image.tmdb.org/t/p/w300'.$poster;
+	// 		}
+	// 		echo '<li><a href="tvshow.php?id=' . $id . '"><img src="'.$poster.'"><h4>'.$title.'</h4></a></li>';
+	// 		}
+	// 	}
+		
+    //     ?>
 		</div>
 		
  <?php
