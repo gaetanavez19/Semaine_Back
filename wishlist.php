2d9a25e67fb6bd7ceae9036a3a2d0fada44a6c92
Noemieponcet <noemieponcet@neuf.fr>
2019-04-11 23:56:39 +0200

    THIS PAGE IS UNDER CONSTRUCTION

--- wishlist.php
+++ wishlist.php
@@ -25,8 +25,34 @@
     //  $movieID = $query->fetchAll();
 ?>
 
-<div>
-<h1>WATHCLIST</h1>
+
+<style>
+    body
+    {
+        height: 100%;
+    }
+
+    .under_construction
+    {
+        height : 82%;
+        display : flex;
+        flex-direction : column;
+        justify-content : center;
+        align-items : center;
+    }
+
+    .under_construction h1 
+    {
+        text-align : center;
+    }
+
+</style>
+
+<div class="under_construction">
+<h1>Sorry <br> This page is under construction</h1>
+<h2>
+    <a class="sitename" href="<?=URL?>index.php">Back home</a>
+</h2>
 </div>
 
 <?php
