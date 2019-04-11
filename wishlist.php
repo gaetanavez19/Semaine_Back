11f0e3633bd409468e76d370130490cb5e4e9908
Noemieponcet <noemieponcet@neuf.fr>
2019-04-11 19:34:35 +0200

    test

--- wishlist.php
+++ wishlist.php
@@ -1,28 +1,33 @@
 <?php 
     require './database/database.php';
-    include 'movie.php';
-     // recuper toutes les données
+    require 'header.php';
+    require 'footer.php';
 
-     if(!empty($_POST))
-     {
-         $data = [
-             'movieID' => trim($_POST['movieID']),
-             'username' => trim($_POST['username']),
-         ];   
-         // envoyer dans SQL
-         $prepare = $pdo->prepare('
-             INSERT INTO
-                 watchlist (movieID, username)
-             VALUES
-                 (:movieID, :username)
-         ');
-         $prepare->execute($data);
-     }  
+    //  // recuper toutes les données
 
-     $query = $pdo->query('SELECT * FROM watchlist WHERE username="'.$_SESSION['username'].'"');
-     $movieID = $query->fetchAll();
-     echo '<pre>';
-     print_r($movieID);
-     echo '</pre>';
+    //  if(!empty($_POST))
+    //  {
+    //      $data = [
+    //          'movieID' => trim($_POST['movieID']),
+    //          'username' => trim($_POST['username']),
+    //      ];   
+    //      // envoyer dans SQL
+    //      $prepare = $pdo->prepare('
+    //          INSERT INTO
+    //              watchlist (movieID, username)
+    //          VALUES
+    //              (:movieID, :username)
+    //      ');
+    //      $prepare->execute($data);
+    //  }  
+
+    //  $query = $pdo->query('SELECT * FROM watchlist WHERE username="'.$_SESSION['username'].'"');
+    //  $movieID = $query->fetchAll();
+    //  echo '<pre>';
+    //  print_r($movieID);
+    //  echo '</pre>';
 ?>
+<div>
+<h1>WATHCLIST</h1>
+</div>
 
